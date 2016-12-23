6aa1e27dabf7125e2f2ec386011caca15012b8a9
Sudarshan Ravindranath <sudarshan.ravindranath@gmail.com>
2016-12-22 18:58:00 -0800

    Initial timer commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var Nav = require('Nav');
+var Main = (props) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <Nav/>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Main;
---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+const PORT = process.env.PORT || 3000;
+app.use(function(request, response, next){
+  if(request.headers['x-forwarded-proto'] === 'https') {
+    response.redirect('http://' + request.hostname + request.url);
+  } else {
+    next();
+  }
+});
+app.use(express.static('public'));
+app.listen(PORT,function(){
+  console.log('Express server '+PORT+' is up');
+})
