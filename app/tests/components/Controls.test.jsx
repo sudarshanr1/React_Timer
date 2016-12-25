45718036adad2d07e57d9e16aab3fa861dcbda9d
Sudarshan Ravindranath <sudarshan.ravindranath@gmail.com>
2016-12-25 15:26:48 -0800

    controls component with test

---
+++ app/tests/components/Controls.test.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jquery');
+var TestUtils = require('react-addons-test-utils');
+
+var Controls = require('Controls');
+
+describe('Controls', function() {
+  it("should exist", function() {
+    expect(Controls).toExist();
+  });
+
+  describe('render', function() {
+    it('should render pause when started', function(){
+      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started"/>);
+      var $el = $(ReactDOM.findDOMNode(controls));
+      var $pauseButton = $el.find('button:contains(Pause)');
+      expect($pauseButton.length).toBe(1);
+    });
+  });
+  describe('render when paused', function() {
+    it('should render started when paused', function(){
+      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused"/>);
+      var $el = $(ReactDOM.findDOMNode(controls));
+      var $pauseButton = $el.find('button:contains(Start)');
+      expect($pauseButton.length).toBe(1);
+    });
+  });
+});
